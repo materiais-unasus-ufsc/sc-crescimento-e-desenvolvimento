5e44c52402008b7664121e62ecc3645cc08c84f7
oasjs <oasjatoba1@gmail.com>
2024-12-08 00:43:29 -0300

    Adiciona o main.js como ponto de entrada para os scripts

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Main script file.
+ * @module main
+ */
+
+import * as common from '../sc-common/js/common.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+  common.loadFigures();
+  common.loadVideos();
+  common.loadComponents();
+  common.loadCallouts();
+  common.setUpPopovers();
+
+  document.addEventListener
+});
+
