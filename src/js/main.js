a5ddb915397fd61cbd415632fd04cea78d4ea053
oasjs <oasjs@proton.me>
2025-01-29 16:42:25 -0300

    Move mainURL and pages list to course specific script file

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,17 @@
 import * as common from "../sc-common/js/common.js";
 import * as helper from "../sc-common/js/helper.js";
 
+const mainURL = "https://unasus-cp.moodle.ufsc.br/course/view.php?id=542";
+const un1qaURL = "https://unasus-cp.moodle.ufsc.br/mod/quiz/view.php?id=9059";
+const pages = [
+  "sobre.html",
+  "un1.html",
+  un1qaURL,
+  "un2.html",
+  "un3.html",
+  "un4.html",
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   common.loadFigures();
   common.loadVideos();
@@ -16,5 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener;
 });
 
-export const navigate = common.navigate;
+export function navigate(option) {
+  common.navigate(option, mainURL, pages);
+}
+
 window.showSelector = helper.showSelector;
